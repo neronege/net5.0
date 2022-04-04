87b6ae57f54e15d9b25e7a21ea6a9a57c53099d9
neronege <mustafaegemenozmen@gmail.com>
2022-04-04 16:40:33 +0300

    first commit

---
+++ Data/AppIdentityDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityWT.Data
+{
+    public class AppIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+           
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder)
+        }
+    }
+}
---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityWT.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
