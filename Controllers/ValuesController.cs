c455fba5dedb6d4ba17fded250d29bfa46e83811
Egemen Ozmen <72514229+neronege@users.noreply.github.com>
2022-04-14 14:41:01 +0300

    Add files via upload

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,18 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JWT.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace IdentityWT.Controllers
+namespace JWT.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private AppIdentityDbContext context;
+
+        public ValuesController(AppIdentityDbContext context)
+        {
+            this.context = context;
+        }
+
         // GET: api/<ValuesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            //return new string[] { "value1", "value2" };
+            return context.Users.Select(x => x.UserName).ToArray();
         }
 
         // GET api/<ValuesController>/5
